36909f64f82dfd078a9e85c3acf05c99b163c654
dttg-nguyen <dttg-nguyen@users.noreply.github.com>
2022-02-11 00:15:31 -0500

    Add onclick listener for History button

--- app/src/main/java/com/example/cashregister/ManagerPanel.java
+++ app/src/main/java/com/example/cashregister/ManagerPanel.java
@@ -1,15 +1,33 @@
 package com.example.cashregister;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class ManagerPanel extends AppCompatActivity {
+public class ManagerPanel extends AppCompatActivity implements View.OnClickListener {
+    Button historyButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_manager_panel);
 
+        historyButton = findViewById(R.id.history_button);
+        historyButton.setOnClickListener(this);
     }
 
+    @Override
+    public void onClick(View view) {
+        int id = view.getId();
+
+        switch (id){
+            case R.id.history_button:
+                Intent intent;
+                intent = new Intent(ManagerPanel.this, HistoryAdapterActivity.class);
+                startActivity(intent);
+        }
+    }
 }
