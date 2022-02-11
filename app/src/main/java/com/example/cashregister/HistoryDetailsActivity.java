08ca6ebe390a61367875c0218b77d9edaace605e
dttg-nguyen <dttg-nguyen@users.noreply.github.com>
2022-02-11 17:46:11 -0500

    Add layout and activity for History Details

---
+++ app/src/main/java/com/example/cashregister/HistoryDetailsActivity.java
@@ -0,0 +1,29 @@
+package com.example.cashregister;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class HistoryDetailsActivity extends AppCompatActivity {
+    TextView productName;
+    TextView totalPrice;
+    TextView date;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.history_details);
+
+        productName = findViewById(R.id.history_details_productName);
+        totalPrice = findViewById(R.id.history_details_totalPrice);
+        date = findViewById(R.id.history_details_date);
+
+        // Retrieve history details from intent passed by History Activity
+        Intent intent = getIntent();
+        productName.setText("Product: " + intent.getStringExtra("name"));
+        totalPrice.setText("Price: " + intent.getStringExtra("totalPrice"));
+        date.setText("Purchase Date: " + intent.getStringExtra("date"));
+    }
+}
