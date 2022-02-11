0d56ecbfeca44d3970c05486304448f7d1e4240c
dttg-nguyen <dttg-nguyen@users.noreply.github.com>
2022-02-11 17:41:58 -0500

    Update HistoryManager

--- app/src/main/java/com/example/cashregister/HistoryManager.java
+++ app/src/main/java/com/example/cashregister/HistoryManager.java
@@ -1,9 +1,14 @@
 package com.example.cashregister;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class HistoryManager {
     List<History> historyList;
+
+    public HistoryManager(){
+        historyList = new ArrayList<>();
+    }
 
     public void add(History history){
         historyList.add(history);
