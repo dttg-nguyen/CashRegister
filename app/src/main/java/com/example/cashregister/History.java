07e5ab98da64ed85a811e3e23c3e20659fce87eb
dttg-nguyen <dttg-nguyen@users.noreply.github.com>
2022-02-14 23:23:24 -0500

    Add getter, setter functions for the model

--- app/src/main/java/com/example/cashregister/History.java
+++ app/src/main/java/com/example/cashregister/History.java
@@ -3,15 +3,31 @@ package com.example.cashregister;
 import java.util.Date;
 
 public class History {
-    String productName;
-    String quantity;
-    Double totalPrice;
-    Date purchaseDate;
+    private final String productName;
+    private final String quantity;
+    private final double totalPrice;
+    private final Date purchaseDate;
 
-    public History(String name, String qty, Double total, Date date) {
+    public History(String name, String qty, double total, Date date) {
         productName = name;
         quantity = qty;
         totalPrice = total;
         purchaseDate = date;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public String getQuantity() {
+        return quantity;
+    }
+
+    public double getTotalPrice() {
+        return totalPrice;
+    }
+
+    public Date getPurchaseDate() {
+        return purchaseDate;
     }
 }
--- app/src/main/java/com/example/cashregister/Product.java
+++ app/src/main/java/com/example/cashregister/Product.java
@@ -1,13 +1,29 @@
 package com.example.cashregister;
 
 public class Product {
-    String name;
-    int quantity;
-    double price;
+    private final String name;
+    private int quantity;
+    private final double price;
 
     public Product(String name, int quantity, double price){
         this.name = name;
         this.quantity = quantity;
         this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setQuantity(int quantity){
+        this.quantity = quantity;
     }
 }
